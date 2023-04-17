098be91a73a52b269bbe375bde90defc0b45e585
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-04-17 17:06:20 +0200

    funcion basica

--- arachnida.py
+++ arachnida.py
@@ -6,47 +6,74 @@
 #    By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/17 13:09:09 by tvillare          #+#    #+#              #
-#    Updated: 2023/04/17 14:11:49 by tvillare         ###   ########.fr        #
+#    Updated: 2023/04/17 17:04:53 by tvillare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import requests
 import os
-import urllib.request
 from bs4 import BeautifulSoup
 import unicodedata
+import time
 
 
-def	descargar_img(url):
-	#img = soup3.find(class_= "vtex-store-components-3-x-productImageTag vtex-store-components-3-x-productImageTag--main")
-	#print(url)
-	img_url= (url.get( 'src' ))
-	if (len(img_url) > 2):
+def	pull_img(imgs):
+	for url in imgs:
+		img_url= (url.get( 'src' ))
+		if (len(img_url) > 2):
+			try:
+				name = img_url.split("/")
+				if (len(name[-1]) < 0):
+					return
+				ext = name[-1].split(".")
+				#print(ext[-1])
+				if (ext[-1] == "jpg" or ext[-1]  == "jpeg" or ext[-1]  == "png" or ext[-1]  == "gif" or ext[-1]  == "bmp"):
+					#print(">>",name[-1])
+					nom_img = "./img/" +  unicodedata.normalize('NFKD', name[-1]).encode('ASCII', 'ignore').decode('utf-8')
+					respuesta = requests.get(img_url, stream=True)
+					respuesta.raise_for_status()
+					with open(nom_img, "wb") as archivo:
+						for chunk in respuesta.iter_content(chunk_size=8192):
+							archivo.write(chunk)
+					#print("Imagen descargada y guardada como:", nom_img)
+			except:
+				print(tmp, "\n", name[-1])
+
+
+def	find_url(soup, org):
+	url = soup.find_all("a")
+	imgs = soup.find_all("img")
+	pull_img(imgs)
+	blacklist = []
+	for link in url:
 		try:
-			name = img_url.split("/")
-			if (len(name[-1]) < 2):
-				return
-			tmp = unicodedata.normalize('NFKD', name[-1]).encode('ASCII', 'ignore').decode('utf-8')
-			nom_img = "./img/" + tmp
-			respuesta = requests.get(img_url, stream=True)
-			respuesta.raise_for_status()
-			with open(nom_img, "wb") as archivo:
-				for chunk in respuesta.iter_content(chunk_size=8192):
-					archivo.write(chunk)
-			print("Imagen descargada y guardada como:", nom_img)
+			link_url = (link.get( 'href' ))
+			if (org in link_url) and (org != link_url) and not(link_url in blacklist):
+				print("-->",link_url)
+				blacklist.append(link_url)
+				#print("-->",link_url, "\n", link)
+				tmp_page = requests.get(link_url)
+				tmp_soup = BeautifulSoup(tmp_page.content, "html.parser")
+				find_url(tmp_soup, link_url)
 		except:
-			print(tmp, "\n", name[-1])
+			continue
 
 
 if not os.path.exists('img'):
 	os.mkdir("img")
-page = requests.get( "https://www.42madrid.com/")
+blacklist = []
+org = "https://www.42barcelona.com/es"
+print("-->",org)
+page = requests.get(org)
 soup = BeautifulSoup(page.content, "html.parser")
-imgs = soup.find_all("img")
-print(*imgs, sep="\n")
-for x in imgs:
-	descargar_img(x)
+#blacklist.append(url)
+find_url(soup, org)
+
+#imgs = soup.find_all("img")
+#print(imgs, sep="\n")
+#pull_img(imgs)
 #urllib.request.urlretrieve(img_url,nom_img)
-print("----------------")
+#print("----------------")
 #url = soup.find_all("a")
 #print(*url, sep="\n")
+
