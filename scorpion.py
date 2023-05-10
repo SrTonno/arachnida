54b496f8fecdb3b7725da6f48322c40b1751ae5d
SrTonno <tvillare@student.42madrid.com>
2023-05-10 20:14:22 +0200

    new scorpion

--- scorpion.py
+++ scorpion.py
@@ -6,41 +6,31 @@
 #    By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/19 14:56:24 by tvillare          #+#    #+#              #
-#    Updated: 2023/04/20 16:05:38 by tvillare         ###   ########.fr        #
+#    Updated: 2023/05/10 18:31:43 by tvillare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from PIL import Image
-from PIL.ExifTags import TAGS
+
 import os
 import sys
 
+import exiftool
+#installar conda install -c conda-forge exiftool
+def leer_metadatos(archivo):
+	with exiftool.ExifToolHelper() as et:
+		metadata = et.get_metadata(archivo)
+		for item in metadata:
+			for etiqueta, valor in item.items():
+				print("{:<40}:\t{:<30}".format(etiqueta, valor))
 
-# Abrir la imagen
-#img = Image.open('/Users/tvillare/Desktop/IMG_20170429_110054.jpg')
-
-# Obtener los metadatos de la imagen
-def get_meta(data):
-	img = Image.open(data)
-	exif = img._getexif()
-	if exif:
-		for tag_id in exif:
-			try:
-				tag = TAGS.get(tag_id, tag_id)
-				data = exif.get(tag_id)
-				if isinstance(data, bytes):
-					data = data.decode()
-				print("{:<30}:\t{:>20}".format(tag, data))
-			except:
-				continue
-	else:
-		print("La imagen no tiene metadatos EXIF.")
-
+def eliminar_metadatos(archivo, etiquetas):
+	with ExifTool() as et:
+		et.delete_tags(archivo, etiquetas)
 
 a = sys.argv[1:]
 for x in a:
 	print("\n->",x)
 	try:
-		get_meta(x)
+		meta = leer_metadatos(x)
 	except:
 		print("ERROR name img")
